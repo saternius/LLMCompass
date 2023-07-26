5bad115eb03cc1a9e48ec7f265ddae9868ead43a
Anil Jason <anil@quillbot.com>
2023-07-25 23:36:37 -0500

    oki

--- local-site/src/pages/InferencePage.js
+++ local-site/src/pages/InferencePage.js
@@ -5,7 +5,8 @@ class InferencePage extends React.Component {
       constructor(props){
         super(props);
         this.state = {
-          valFunc: 'mean'
+          valFunc: 'mean',
+          focus: null
         }
 
         AppModel.setUpdater(()=>{
@@ -21,6 +22,30 @@ class InferencePage extends React.Component {
           return sum/arr.length
         }
 
+        if(this.state.valFunc === "min"){
+          let min = 10000
+          arr.forEach(x=>{min = Math.min(min, x[1])})
+          return min
+        }
+
+        if(this.state.valFunc === "max"){
+          let max = 10000
+          arr.forEach(x=>{max = Math.max(max, x[1])})
+          return max
+        }
+
+        if(this.state.valFunc === "median"){
+          var vals = arr.map(x=>x[1])
+          vals = vals.sort()
+          return vals[Math.floor(vals.length/2)]
+        }
+
+        if(this.state.valFunc === "sum"){
+          let sum = 0
+          arr.forEach(x=>{sum += x[1]})
+          return sum
+        }
+
       }
 
       renderMatrix(){
@@ -29,12 +54,33 @@ class InferencePage extends React.Component {
         var k = 0
         var z = 0
 
+
+      function interpolateColor(color1, color2, factor) {
+        if (arguments.length < 3) { 
+            factor = 0.5; 
+        }
+        var result = color1.slice();
+        for (var i = 0; i < 3; i++) {
+            result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+        }
+        return result;
+      };
+      
+      function interpolateColors(color1, color2, factor) {
+          var color1 = color1.match(/\d+/g).map(Number); // convert color1 to array [r, g, b]
+          var color2 = color2.match(/\d+/g).map(Number); // convert color2 to array [r, g, b]
+          var result = interpolateColor(color1, color2, factor);
+          return 'rgb(' + result.join(',') + ')'; // convert array to string
+      }
+
         var max = 0
+        var min = 1000
         AppModel.matrix.forEach(row=>{
           row.forEach(cell=>{
-              console.log(cell)
+             // console.log(cell)
               var val = this.getValue(cell.likeness);
               max = Math.max(val, max)
+              min = Math.min(val, min)
           })
         })
 
@@ -46,15 +92,16 @@ class InferencePage extends React.Component {
                 row.map(c=>{
                   z += 1
                   var val = this.getValue(c.likeness);
-                  var green = 255*(val/max)
-                  var blue = 255*(val/max)
-                  var red = 255*(val/max)
-                  var color = `rgb(${red}, ${green}, ${blue})`
+                  var c1 = "rgb(215,22,0)"
+                  var c2 = "rgb(220,223,100)"
+                  var color = interpolateColors(c2, c1, (val-min)/(max-min))
                   return (
-                        <div style={{background:color}} key={'matrix-'+z} className='matrix-cell'>
+                        <div style={{background:color}} key={'matrix-'+z} className='matrix-cell' onClick={()=>{
+                          this.setState({focus:c.likeness})
+                        }}>
                           <div>{c.economic}</div>
                           <div>{c.social}</div>
-                          <div>{Math.floor(green)}</div>
+                          <div>{val.toString().slice(0, 8)}</div>
 
                         </div>
                     )
@@ -70,12 +117,55 @@ class InferencePage extends React.Component {
           </div>
           )
       }
+
+
+      renderScaleFns(){
+        var btns = ["max", "min", "mean", "median", "sum"]
+        return (
+          <div className='scale-btns'>
+            {btns.map(b=>{
+              return (<div className='scale-btn' key={'btn-'+b} onClick={()=>{
+                this.setState({valFunc:b})
+              }}>{b}</div>)
+            })}
+          </div>
+        )
+      }
+
+
+      renderFocus(){
+        if(!this.state.focus) return null
+        let i = 0
+        console.log(this.state.focus)
+        return (
+          <div className='focus-container'>
+            {this.state.focus.map(x=>{
+              i += 1
+              console.log(x)
+              return (
+                <div className='f-span' key={'f-word-'+i}>
+                  <div className='f-token'>
+                    {x[0]}
+                  </div>
+                  <div className='f-val'>
+                    {x[1]}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )
+      }
+
+
       render() {
         return (
           <div className='monitor'>
               <div className="title-desc">Write a statement and hit Enter to see how probable it is (LLAMA2-13B)</div>
               <input id='takeInput' className="take-input" placeholder={AppModel.placeholder} type="text"/>
+              {this.renderScaleFns()}
               {this.renderMatrix()}
+              {this.renderFocus()}
           </div>
         );
       }
